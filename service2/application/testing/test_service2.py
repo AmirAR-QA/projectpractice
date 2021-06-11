fa76bff93190d3a312d634d34fa511ebab11ce47
AmirAR-QA <aabdur-rahman@academytrainee.com>
2021-06-11 11:57:37 +0100

    test3

---
+++ service2/application/testing/test_service2.py
@@ -0,0 +1,19 @@
+from flask_testing import TestCase
+from flask import url_for
+import requests
+from requests_mock import mock
+from application import app, encounter
+from application.models import Form
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(DEBUG = True)
+        return app
+
+class TestQuery(TestCase):
+    def test_service2(self):
+        
+        response = self.client.get(url_for("encounter"))
+
+        allencounters = ["a giant rat","a giant goat","a pack of firebreathing lizards","a very small dwarf","an evil pig summoning wizard"]
+        self.assertIn(response.data.decode(), allencounters)
